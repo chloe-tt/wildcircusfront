85d58f8bfbd32984610c0cdc52ff8c3485fd1e11
chloe-tt <chloe.trottin.chine@gmail.com>
2020-07-22 19:47:54 +0200

    route authentication fixed

--- back/routes/users.js
+++ back/routes/users.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const connection = require('../config')
 const bcrypt = require('bcrypt')
+const jwt = require("jsonwebtoken");
 
 // GET ALL NEWS CREATED BY A SPECIFIC USER
 
